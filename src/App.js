45c09e82900c35f903a7c6b74e80626d9a1c2afc
yurivlm <yurivictor.lm@gmail.com>
2020-12-22 17:58:00 -0300

    Nome na capa

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <BannerMain
     videoTitle = {dadosIniciais.categorias[0].videos[0].titulo}
    url={dadosIniciais.categorias[0].videos[0].url}
-   videoDescription={"O que é Front-end? Trabalhando na área."}
+   videoDescription={"O que é Front-end? Trabalhando na área. Yuri Victor"}
 />
 
 <Carousel
