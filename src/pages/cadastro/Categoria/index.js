9d7b2950d87dadb1bcce1c3221c7b1be583bdb3a
yurivlm <yurivictor.lm@gmail.com>
2020-12-26 20:52:09 -0300

    Fim da Aula 2

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,17 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
-function CadastroCategoria(){
-    return (
+function CadastroCategoria() {
+  return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
-    
-    <Link to="/"> 
-    Ir para Home
-    </Link>
-    
-    </PageDefault>
-    )
-    }
 
-    export default CadastroCategoria
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
+}
+
+export default CadastroCategoria;
